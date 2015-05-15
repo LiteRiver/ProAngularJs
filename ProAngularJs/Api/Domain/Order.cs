d0a8a9a64ef05700c7681194797f7117685d4cd2
LiteRiver <zhxchchuan@gmail.com>
2015-05-15 16:52:46 +0800

    订单

---
+++ ProAngularJs/Api/Domain/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProAngularJs.Api.Domain {
+    public class Order {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+
+        private ICollection<Product> m_products;
+        public virtual ICollection<Product> Products {
+            get { return m_products ?? (m_products = new List<Product>()); }
+            set { m_products = value; }
+        }
+    }
+}
--- ProAngularJs/C07-01/controllers/SportsStore.js
+++ ProAngularJs/C07-01/controllers/SportsStore.js
@@ -1,12 +1,26 @@
 ﻿angular.module("SportsStore")
     .constant("ProductsUrl", "/SportsStore/Products")
-    .controller("SportsStoreCtrl", function ($scope, $http, ProductsUrl) {
+    .constant("OrderUrl", "/SportsStore/Orders")
+    .controller("SportsStoreCtrl", function ($scope, $http, ProductsUrl, OrderUrl) {
         $scope.data = {};
-        $http.get(ProductsUrl)
-            .success(function (data) {
-                $scope.products = data;
-            }).error(function (error, errorStatus) {
-                $scope.error = error;
-                $scope.errorStatus = errorStatus;
+        $http.get(ProductsUrl).success(function (data) {
+            $scope.products = data;
+        }).error(function (error, errorStatus) {
+            $scope.error = error;
+            $scope.errorStatus = errorStatus;
+        });
+
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+
+            $http.post(OrderUrl, order).success(function (data) {
+                $scope.orderId = data.id;
+                cart.getProducts().length = 0;
+            }).error(function (error) {
+                $scope.orderError = error;
+            }).finally(function () {
+                $location.path("/complete");
             });
+        };
     });
